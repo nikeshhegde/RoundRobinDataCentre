4b09f865ed6f3c37688e90b2c1ea3c55230aac7b
Nikesh <nhegde05@gmail.com>
2019-11-24 20:50:53 +0000

    new changes

--- RoundRobinDataCentreImplementation/src/com/datacentre/impl/DataCentre.java
+++ RoundRobinDataCentreImplementation/src/com/datacentre/impl/DataCentre.java
@@ -6,9 +6,9 @@ public class DataCentre {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean flag = true;
-		if(flag)
+		if(flag) {
 		System.out.println("Hello World");
-
+		}
 	}
 
 }
