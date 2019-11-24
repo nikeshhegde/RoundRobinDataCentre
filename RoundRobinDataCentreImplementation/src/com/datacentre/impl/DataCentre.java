82a7a523d35eb4c48465caef4c5d07bd495067c0
ameya01 <ameya01.adhyapak@gmail.com>
2019-11-24 20:53:17 +0000

    Testing

--- RoundRobinDataCentreImplementation/src/com/datacentre/impl/DataCentre.java
+++ RoundRobinDataCentreImplementation/src/com/datacentre/impl/DataCentre.java
@@ -5,9 +5,12 @@ public class DataCentre {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		boolean flag = true;
+		if(flag) {
 		System.out.println("Hello World");
-
+		System.out.println("Hello World");
+		
+		}
 	}
 
 }
