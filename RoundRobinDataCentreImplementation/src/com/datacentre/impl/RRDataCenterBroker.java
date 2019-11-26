993491570769a388d1aa5e509b1cd2042bfef496
ameya01 <ameya01.adhyapak@gmail.com>
2019-11-26 15:00:24 +0000

    Some functions for Broker

--- RoundRobinDataCentreImplementation/src/com/datacentre/impl/RRDataCenterBroker.java
+++ RoundRobinDataCentreImplementation/src/com/datacentre/impl/RRDataCenterBroker.java
@@ -1,5 +1,63 @@
 package com.datacentre.impl;
 
-public class RRDataCenterBroker {
+import java.util.List;
+
+import org.cloudbus.cloudsim.DatacenterBroker;
+import org.cloudbus.cloudsim.DatacenterCharacteristics;
+import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.core.CloudSimTags;
+import org.cloudbus.cloudsim.core.SimEvent;
+
+/**This broker allocates VMs to data center following the
+ * <a href="http://en.wikipedia.org/wiki/Round-robin_scheduling">Round-robin</a> algorithm.
+ * @author ameya
+ **/
+
+public class RRDataCenterBroker extends DatacenterBroker 
+{
+	/**
+     * Creates an instance of this class associating to it a given name.
+     * @param name The name to be associated to this broker. It might not be <code>null</code> or empty.
+     * @throws Exception If the name contains spaces.
+     */
+	public RRDataCenterBroker(String name) throws Exception 
+	{
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	protected void processResourceCharacteristics(SimEvent ev) 
+	{
+		DatacenterCharacteristics characterstics = (DatacenterCharacteristics) ev.getData();
+		getDatacenterCharacteristicsList().put(characterstics.getId(),characterstics);
+		
+		if(getDatacenterCharacteristicsList().size() == getDatacenterIdsList().size())
+		{
+			distributeReqForNewVmsAcrossDatacentersUsingRR();
+		}
+	}
+	
+	/**
+     * Distributes the VMs across the data centers using the round-robin approach. A VM is allocated to a data center only if there isn't  
+     * a VM in the data center with the same id.     
+     */
+	
+	protected void distributeReqForNewVmsAcrossDatacentersUsingRR() 
+	{
+		// TODO Auto-generated method stub
+		int numOfVmsAllocated = 0;
+		int i= 0;
+		
+		final List<Integer> availableDatacenters= getDatacenterIdsList();
+		
+		
+		
+		
+		
+	}
+	
+	
 
 }
